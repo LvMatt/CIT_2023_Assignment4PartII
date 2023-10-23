7f2e4159aebfad3c5a4ab4265284dbfc6be57efd
Matúš Kalanin <mato.kalanin@gmail.com>
2023-10-23 23:19:38 +0200

    Create ProductsController.cs

---
+++ WebServer/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer;
+using DataLayer.YourOutputDirectory;
+using Microsoft.AspNetCore.Mvc;
+using WebServer.Models;
+
+namespace WebServer.Controllers;
+
+[Route("api/categories")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+
+    private readonly IDataService _dataService;
+    private readonly LinkGenerator _linkGenerator;
+
+    public ProductsController(IDataService dataService, LinkGenerator linkGenerator)
+    {
+        _dataService = dataService;
+        _linkGenerator = linkGenerator;
+    }
+
+
+}
